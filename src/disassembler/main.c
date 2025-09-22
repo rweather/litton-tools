3eaa6fce8d611deb89546dd7e81944bc3022b736
Rhys Weatherley <rhys.weatherley@gmail.com>
2025-09-22 19:51:12 +1000

    Disassembler mode to straighten out rotation-optimised code

--- src/disassembler/main.c
+++ src/disassembler/main.c
@@ -30,6 +30,7 @@ static uint8_t use_mask[LITTON_DRUM_MAX_SIZE];
 
 static void disassemble_raw(void);
 static void disassemble_pretty(void);
+static void disassemble_straighten(void);
 
 int main(int argc, char *argv[])
 {
@@ -37,6 +38,7 @@ int main(int argc, char *argv[])
     int exit_status = 0;
     int arg;
     int pretty = 1;
+    int straighten = 0;
 
     /* Check for raw or pretty mode */
     if (argc > 1 && !strcmp(argv[1], "--raw")) {
@@ -44,10 +46,29 @@ int main(int argc, char *argv[])
         ++argv;
         --argc;
     }
+    if (argc > 1 && !strcmp(argv[1], "--pretty")) {
+        pretty = 1;
+        ++argv;
+        --argc;
+    }
+    if (argc > 1 && !strcmp(argv[1], "--straighten")) {
+        straighten = 1;
+        pretty = 1;
+        ++argv;
+        --argc;
+    }
 
     /* Need at least one argument */
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s [--raw] input.drum ...\n", progname);
+        fprintf(stderr, "Usage: %s [--raw|--pretty|--straighten] input.drum ...\n", progname);
+        fprintf(stderr, "\n");
+        fprintf(stderr, "    --raw\n");
+        fprintf(stderr, "        Disassemble in raw format.\n\n");
+        fprintf(stderr, "    --pretty\n");
+        fprintf(stderr, "        Disassemble in pretty format (this is the default).\n\n");
+        fprintf(stderr, "    --straighten\n");
+        fprintf(stderr, "        Disassemble in pretty format but rearrange the code to\n");
+        fprintf(stderr, "        straighten out the flow of control.\n");
         return 1;
     }
 
@@ -58,7 +79,9 @@ int main(int argc, char *argv[])
             if (argc > 2) {
                 printf("\n%s:\n", argv[arg]);
             }
-            if (pretty) {
+            if (straighten) {
+                disassemble_straighten();
+            } else if (pretty) {
                 disassemble_pretty();
             } else {
                 disassemble_raw();
@@ -79,6 +102,10 @@ static int is_valid_instruction_word(litton_word_t word)
     unsigned posn = 0;
     uint16_t insn;
     int last_nop = 0;
+    if ((word & 0xFFFF000000ULL) == 0) {
+        /* Instruction starts with $0000, so it may just be a literal */
+        return 0;
+    }
     while (posn < 4) {
         insn = (word >> ((3 - posn) * 8)) & 0xFF;
         ++posn;
@@ -265,9 +292,18 @@ static int is_alpha_numeric(litton_word_t word)
 {
     int bit;
     uint8_t value;
+    const char *string_form;
+    if (word == 0) {
+        return 0;
+    }
     for (bit = 32; bit >= 0; bit -= 8) {
-        value = ((uint8_t)(word >> bit)) & 0x7F;
+        value = ((uint8_t)(word >> bit)) & 0xFF;
         if (value >= 0x40) {
+            return 0;
+        }
+        litton_char_from_charset
+            (value, LITTON_CHARSET_EBS1231, &string_form);
+        if (string_form[0] == '[' || string_form[0] < 0x20) {
             return 0;
         }
     }
@@ -303,7 +339,7 @@ static void disassemble_pretty(void)
                (unsigned)((word >> 8) & 0xFF),
                (unsigned)(word & 0xFF));
         if (is_alpha_numeric(word)) {
-            /* This may be alphanumeric data in the EBC1231 character set */
+            /* This may be alphanumeric data in the EBS1231 character set */
             printf("    \"");
             print_alpha_numeric(word);
             printf("\"\n");
@@ -315,5 +351,219 @@ static void disassemble_pretty(void)
         } else {
             printf("     DW $%010lX\n", (unsigned long)word);
         }
+    }
+}
+
+static uint8_t visited[LITTON_DRUM_MAX_SIZE];
+
+#define VISIT_NONE 0
+#define VISIT_DONE 1
+#define VISIT_SUBROUTINE 2
+
+static void disassemble_visit(litton_drum_loc_t addr)
+{
+    litton_word_t word;
+    litton_drum_loc_t next_addr;
+    const litton_opcode_info_t *opcode;
+    unsigned posn;
+    uint16_t insn;
+    int first;
+    for (;;) {
+        /* Get the next word and mark it as done */
+        word = machine.drum[addr];
+        visited[addr] = VISIT_DONE;
+
+        /* Dump the address and word in hexadecimal */
+        printf("%03X: %02X %02X %02X %02X %02X", addr,
+               (unsigned)((word >> 32) & 0xFF),
+               (unsigned)((word >> 24) & 0xFF),
+               (unsigned)((word >> 16) & 0xFF),
+               (unsigned)((word >> 8) & 0xFF),
+               (unsigned)(word & 0xFF));
+        if (is_alpha_numeric(word)) {
+            /* This may be alphanumeric data in the EBS1231 character set */
+            printf("  \"");
+            print_alpha_numeric(word);
+            printf("\"  ");
+        } else {
+            printf("           ");
+        }
+        first = 1;
+
+        /* Does the word look like an instruction or data word? */
+        if (!is_valid_instruction_word(word)) {
+            /* Data word; dump it as "DW" and we're done */
+            printf("DW    $%010lX\n", (unsigned long)word);
+            break;
+        }
+
+        /* Disassemble the instructions in the word */
+        next_addr = LITTON_DRUM_MAX_SIZE;
+        posn = 0;
+        while (posn < 4) {
+            insn = (word >> ((3 - posn) * 8)) & 0xFF;
+            ++posn;
+            if (insn >= 0x0040) {
+                insn <<= 8;
+                    insn |= (word >> ((3 - posn) * 8)) & 0xFF;
+                ++posn;
+            }
+            if (insn == LOP_NN) {
+                /* Don't bother with no-op's as they are usually padding for
+                 * when the next instruction doesn't fit in the current word. */
+                continue;
+            }
+            opcode = litton_opcode_by_number(insn);
+            if ((insn & 0xF000) == LOP_JU) {
+                /* Explicit jump which we treat as the next address.
+                 * There is no point disassembing the rest of the word
+                 * as the following instructions will never be reached. */
+                next_addr = insn & 0xFFF;
+                break;
+            }
+            if ((insn & 0xF000) == LOP_JM) {
+                /* "Jump Mark" is usually jumping to a subroutine.  Mark the
+                 * subroutine for later if we have not visited it yet. */
+                litton_drum_loc_t subr_addr = insn & 0xFFF;
+                if (visited[subr_addr] == VISIT_NONE) {
+                    visited[subr_addr] = VISIT_SUBROUTINE;
+                }
+            }
+            if ((insn & 0xF000) == LOP_JC && posn >= 4) {
+                /*
+                 * It is common in I/O code to have a busy wait loop like this:
+                 *
+                 *   this:
+                 *       OI $NN
+                 *       JC next
+                 *       JU this
+                 *   next:
+                 *
+                 * Recognise this form and treat "next" as the next address
+                 * to be visited after this word.  Otherwise we will stop at
+                 * "JU this" on the current visit iteration.
+                 */
+                if ((word >> 32) == (addr & 0xFF) &&
+                        visited[insn & 0xFFF] == VISIT_NONE) {
+                    next_addr = insn & 0xFFF;
+                    if (first) {
+                        printf("%-5s", "JC");
+                        first = 0;
+                    } else {
+                        printf("                              %-5s", "JC");
+                    }
+                    printf(" $%03X\n", insn & 0xFFF);
+                    printf("                              %-5s", "JU");
+                    printf(" $%03X\n", addr);
+                    break;
+                }
+            }
+            if (first) {
+                printf("%-5s", opcode->name);
+                first = 0;
+            } else {
+                printf("                              %-5s", opcode->name);
+            }
+            switch (opcode->operand_type) {
+            case LITTON_OPERAND_NONE:
+                printf("\n");
+                break;
+
+            case LITTON_OPERAND_MEMORY:
+                if ((insn & 0xFFF) < 8) {
+                    /* Probably a scratchpad register address */
+                    printf(" %d\n", insn & 0xFFF);
+                } else {
+                    printf(" $%03X\n", insn & 0xFFF);
+                }
+                break;
+
+            case LITTON_OPERAND_SCRATCHPAD:
+            case LITTON_OPERAND_HALT:
+                printf(" %d\n", insn & 0x07);
+                break;
+
+            case LITTON_OPERAND_SHIFT:
+                printf(" %d\n", (insn & 0x7F) + 1);
+                break;
+
+            case LITTON_OPERAND_DEVICE:
+            case LITTON_OPERAND_CHAR:
+                printf(" $%02X\n", insn & 0xFF);
+                break;
+            }
+        }
+
+        /* Determine the address of the next instruction to visit.
+         * If it has already been visited, jump to it and stop.
+         * Otherwise fall through and visit it as the new word. */
+        if (next_addr >= LITTON_DRUM_MAX_SIZE) {
+            next_addr = (addr & 0x0F00) | (word >> 32);
+        }
+        if (visited[next_addr] != VISIT_NONE) {
+            if (first) {
+                printf("%-5s $%03X\n", "JU", next_addr);
+            } else {
+                printf("                              %-5s $%03X\n",
+                       "JU", next_addr);
+            }
+            break;
+        }
+        if (first) {
+            /* All instructions were elided for this word.  Add an
+             * explicit jump to the next word so we have something. */
+            printf("%-5s $%03X\n", "JU", next_addr);
+        }
+        addr = next_addr;
+    }
+}
+
+static void disassemble_straighten(void)
+{
+    litton_drum_loc_t addr;
+
+    /* Mark all words as unvisited to begin with */
+    memset(visited, VISIT_NONE, sizeof(visited));
+
+    /* Mark anything that is not in the original drum image as done */
+    for (addr = 0; addr < LITTON_DRUM_MAX_SIZE; ++addr) {
+        if (!use_mask[addr]) {
+            visited[addr] = VISIT_DONE;
+        }
+    }
+
+    /* Start by visiting the entry point if we have one */
+    if (use_mask[machine.entry_point]) {
+        disassemble_visit(machine.entry_point);
+    }
+
+    /* Loop continuously until we can't find anything else to visit.
+     * In the worst case, this exhibits O(n^2) behaviour.  That is,
+     * 4096^2 = 16777216 visits.  Modern computers can handle that. */
+    for (;;) {
+        /* Try finding a subroutine entry point */
+        for (addr = 0; addr < LITTON_DRUM_MAX_SIZE; ++addr) {
+            if (visited[addr] == VISIT_SUBROUTINE) {
+                break;
+            }
+        }
+        if (addr < LITTON_DRUM_MAX_SIZE) {
+            disassemble_visit(addr);
+            continue;
+        }
+
+        /* Find any other address that hasn't been visited yet */
+        for (addr = 0; addr < LITTON_DRUM_MAX_SIZE; ++addr) {
+            if (visited[addr] == VISIT_NONE) {
+                break;
+            }
+        }
+        if (addr >= LITTON_DRUM_MAX_SIZE) {
+            /* Everything has now been visited */
+            break;
+        }
+
+        /* Visit the address */
+        disassemble_visit(addr);
     }
 }
